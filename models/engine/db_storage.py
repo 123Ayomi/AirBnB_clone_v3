72196165ad91881b7cf293d34b5edd8fafa1e659
123Ayomi <otepolabusayo@yahoo.com>
2024-05-02 22:12:52 -0700

    Airbnb clone

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -76,25 +76,17 @@ class DBStorage:
         self.__session.remove()
 
     def get(self, cls, id):
-        """
-        Retrieves object of a class or all objects of that class
-        """
-        if id and isinstance(id, str):
-            if cls and (cls in classes.keys() or cls in classes.values()):
-                all_objs = self.all(cls)
-                for key, value in all_objs.items():
-                    if id == value.id and key.split('.')[1] == id:
-                        return value
-        return
+        """ retrieves """
+        if cls in classes.values() and id and type(id) == str:
+            d_obj = self.all(cls)
+            for key, value in d_obj.items():
+                if key.split(".")[1] == id:
+                    return value
+        return None
 
     def count(self, cls=None):
-        """
-        Returns the occurrence of a class or all classes
-        """
-        occurrence = 0
-        if cls:
-            if cls in classes.keys() or cls in classes.values():
-                occurrence = len(self.all(cls))
-        if not cls:
-            occurrence = len(self.all())
-        return occurrence
+        """ counts """
+        data = self.all(cls)
+        if cls in classes.values():
+            data = self.all(cls)
+        return len(data)
